e37daea4eb8d910a64848f73d8362a2e0f65af50
Riley Watkins <riley@rileywatkins.com>
2013-05-16 22:15:50 -0700

    Transaction validation; replace (reduce conj) with (into)

--- src/hawk/views.clj
+++ src/hawk/views.clj
@@ -1,6 +1,7 @@
 (ns hawk.views
   (:require [clj-time.coerce]
             [clj-time.format]
+            [ring.util.response]
             [hiccup.core :as h]
             [hawk.models.db :as db]))
 
@@ -52,24 +53,23 @@
   (identity [:li [:a {:href (str "/account/" id)} name]]))
 
 (defn accounts-ul [accounts]
-  (reduce conj [:ul] (map account-li accounts)))
+  (into [:ul] (map account-li accounts)))
 
 (defn -display-date [sql-date]
   (let [fmt (clj-time.format/formatter "yyyy-MM-dd")]
     (clj-time.format/unparse fmt (clj-time.coerce/from-sql-date sql-date))))
 
 (defn transactions-ul [transactions]
-  (reduce
-    conj [:ul]
-    (map #(identity
-            [:li (str "[" (:category_name %) "] "
-                      (-display-date (:date %)) ", "
-                      (:memo %)
-                      ", $" (/ (:amount %) 100))])
-         transactions)))
+  (into [:ul]
+        (map #(identity
+                [:li (str "[" (:category_name %) "] "
+                          (-display-date (:date %)) ", "
+                          (:memo %)
+                          ", $" (/ (:amount %) 100))])
+             transactions)))
 
 (defn categories-ul [categories]
-  (reduce conj [:ul] (map #(identity [:li (:name %)]) categories)))
+  (into [:ul] (map #(identity [:li (:name %)]) categories)))
 
 
 ;; Pages
@@ -110,7 +110,8 @@
 
 (defn new-category [{category-name :name}]
   (if (not= (count category-name) 0)
-    (do (db/create-category! {:name category-name})
+    (do
+      (db/create-category! {:name category-name})
       "Created!")
     "Fail"))
 
@@ -123,7 +124,30 @@
     (base-page {:title title :body body})))
 
 (defn parse-int [s]
-  (Integer. (re-find #"\d+" s)))
+  (if (or (nil? s)
+          (zero? (count s)))
+    0
+    (Integer. (re-find #"\d+" s))))
+
+(defn validator-for-key [key]
+  (condp = key
+    :account_id (fn [value]
+                  (when (or (nil? value) (zero? value))
+                    "'account' field is required"))
+    :amount #(println %)
+    :category_id nil
+    :date (fn [value]
+            (when (empty? value) "'date' field is required"))
+    :memo nil))
+
+(defn validate-transaction [t]
+  (loop [errors {} all-keys (keys t) k (first all-keys)]
+    (let [new-error (when-let [validate-fn (validator-for-key k)]
+                      (validate-fn (k t)))
+          errors (if (nil? new-error) errors (assoc errors k new-error))]
+      (if (next all-keys)
+        (recur errors (next all-keys) (first (next all-keys)))
+        (when-not (empty? errors) errors)))))
 
 (defn save-transaction [tr-map]
   (let [amt (parse-int (:amount tr-map))
@@ -131,8 +155,13 @@
                     :amount amt
                     :category_id (parse-int (:category_id tr-map))
                     :account_id (parse-int (:account_id tr-map)))
-        result (db/create-transaction! data)]
-    (str result)))
+        errors (validate-transaction data)]
+    (if (nil? errors)
+      (db/create-transaction! data)
+      {:error (str errors)})))
 
 (defn new-transaction [data]
-  (save-transaction data))
+  (let [res (save-transaction data)]
+    (if-let [error (:error res)]
+      error
+      (ring.util.response/redirect (str "/account/" (:account_id data))))))
